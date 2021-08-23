8deac8fb1f03f39b7db30e046c92f2c29ec4dd8f
RanieryAV <58216614+RanieryAV@users.noreply.github.com>
2021-08-23 17:47:01 -0300

    Implementação de vetor bimensional para as mesas e ajustes das funções para tal estrutura de dados nova.
    - As mesas são agora armazenadas em um vetor bidimensional (ou seja, uma matriz 5x6)
    - Todas as funções (ou quase todas) foram adaptadas para suportarem vetor bidimensional
    - Alguém pode tentar fazer a função criarMesa() funcionar, por favor? Isso resolveria o problema de instanciar as 30 mesas na força bruta.

--- src/gerenciamento/de/mesas/de/restaurante/gmr/GerenciamentoDeMesasDeRestauranteGMR.java
+++ src/gerenciamento/de/mesas/de/restaurante/gmr/GerenciamentoDeMesasDeRestauranteGMR.java
@@ -18,37 +18,22 @@ import static gerenciamento.de.mesas.de.restaurante.gmr.Mesa.estados.TEST;
  */
 public class GerenciamentoDeMesasDeRestauranteGMR {
 
-    public static boolean mesaLivre(Mesa mesa){ //FUNÇÃO PRONTA!
-        if(mesa.estadoAtual == LIVRE && mesa.isReserva() == false){
+    public static boolean mesaLivre(Mesa vetMesa[][], int linha, int coluna){ //FUNÇÃO PRONTA!
+        
+        if(vetMesa[linha][coluna].estadoAtual == LIVRE && vetMesa[linha][coluna].isReserva() == false){
             return true;
         }else{
             return false;
         }
     }
     
-    public static Mesa liberarMesa(Mesa mesa){
-        if(mesa.estadoAtual == DESINFECTAR){
-            desinfectarMesa(mesa);
+    public static void liberarMesa(Mesa vetMesa[][], int linha, int coluna){ //FUNÇÃO PRONTA!
+        if(vetMesa[linha][coluna].estadoAtual == DESINFECTAR){
+            desinfectarMesa(vetMesa, linha, coluna);
         }
-        
-        
-        /*
-        if(mesa.isReserva() == true)
-        {
-            return false;
-        }else{
-            return true;
-        }
-        */
-        
-        /*
-        if(mesa.getEstadoAtual() == DESINFECTAR){
-            desinfectar
-        }
-        */
-        return mesa;
     }
     
+    /*
     public static void criarMesa(Mesa vetor_mesa[], int numeroTotalDeMesas){
         int cont = 0;
         Mesa mesaPlaceHolder; //Foi necessário criar um objeto do tipo Mesa localmente dentro da função porque devido a um detalhe técnico da linguagem Java no qual quando um vetor de um tipo de dado criado pelo programador (ou seja, uma classe) é instanciado, ele AUTOMATICAMENTE tem todos as suas posições iniciadas com NULL (ou seja, um vetor de objetos vazios). Por isso a variável individual mesaPlaceHolder (com todos os campos com valores padrão definidos na classe original) é atibuída a cada posição do vetMesa[].
@@ -65,18 +50,18 @@ public class GerenciamentoDeMesasDeRestauranteGMR {
             vetor_mesa[cont].setNumeroCadeiras(4);
         }
     }
-    
-    public static Mesa desinfectarMesa(Mesa mesaASerLimpa){ //FUNÇÃO PRONTA!
-        if(mesaASerLimpa.estadoAtual == DESINFECTAR){
-            mesaASerLimpa.estadoAtual = LIVRE;
+    */
+    public static void desinfectarMesa(Mesa vetMesa[][], int linha, int coluna){ //FUNÇÃO PRONTA!
+        if(vetMesa[linha][coluna].estadoAtual == DESINFECTAR){
+            vetMesa[linha][coluna].estadoAtual = LIVRE;
         }
-        return mesaASerLimpa;
     }
     
-    public static void distanciamentoMesa(Mesa vetMesa[], int numeroMesa){ //Falta tratar exceções de limiar de numeração das mesas
+    public static void distanciamentoMesa(Mesa vetMesa[][], int numeroMesa){ //Falta tratar exceções de limiar de numeração das mesas
         int cont = 0;
         int idMesa = numeroMesa - 1;//Subtrair 1 considerando que vetor em Java começa em 0 (zero)
-        
+        int i, j;
+        /*
         switch(idMesa){
             case 0:
                 vetMesa[(idMesa + 1)].setEstadoAtual(DISTANCIAMENTO_SOCIAL);//bloquear mesa seguinte
@@ -98,8 +83,8 @@ public class GerenciamentoDeMesasDeRestauranteGMR {
                 vetMesa[(idMesa + 6)].setEstadoAtual(DISTANCIAMENTO_SOCIAL);//bloquear mesa inferior
                 break;
         }
-        
-        
+        */
+        /*
         if(numeroMesa >= 0 && numeroMesa <= 30){
             vetMesa[(idMesa - 1)].setEstadoAtual(DISTANCIAMENTO_SOCIAL);//bloquear mesa anterior
             vetMesa[(idMesa + 1)].setEstadoAtual(DISTANCIAMENTO_SOCIAL);//bloquear mesa seguinte
@@ -108,12 +93,50 @@ public class GerenciamentoDeMesasDeRestauranteGMR {
         }else{
             System.out.println("\nErro: O numero de mesa digitado é invalido!\n");
         }
+        */
+        if(numeroMesa >= 0 && numeroMesa <= 30){
+            for(i = 0; i < 5; i++){
+                 for(j = 0; j < 6; j++){
+                        if(vetMesa[i][j].getNumeroMesa() == numeroMesa){
+                            vetMesa[i][(j - 1)].setEstadoAtual(DISTANCIAMENTO_SOCIAL);//bloquear mesa anterior
+                            vetMesa[i][(j + 1)].setEstadoAtual(DISTANCIAMENTO_SOCIAL);//bloquear mesa seguinte
+                            vetMesa[(i + 1)][j].setEstadoAtual(DISTANCIAMENTO_SOCIAL);//bloquear mesa inferior
+                            vetMesa[(i - 1)][j].setEstadoAtual(DISTANCIAMENTO_SOCIAL);//bloquear mesa superior
+                        }
+                 }
+            }
+        }else{
+            System.out.println("\nErro: O numero de mesa digitado é invalido!\n");
+        }
+    }    
+    /**
+     *
+     * @param vetMesa
+     * @param numeroMesa
+     */
+    public static void ocuparMesa(Mesa vetMesa[][], int numeroMesa){
+        //int idMesa = numeroMesa - 1;//Subtrair 1 considerando que vetor em Java começa em 0 (zero)
+        int i, j;
         
-    }
-    
-    public static void ocuparMesa(Mesa vetMesa[], int numeroMesa){
-        int idMesa = numeroMesa - 1;//Subtrair 1 considerando que vetor em Java começa em 0 (zero)
-        
+        if(numeroMesa >= 0 && numeroMesa <= 30){
+            for(i = 0; i < 5; i++){
+                for(j = 0; j < 6; j++){
+                    if(vetMesa[i][j].getNumeroMesa() == numeroMesa){
+                        if(mesaLivre(vetMesa, i, j) == true){
+                            vetMesa[i][j].setEstadoAtual(OCUPADA);
+                            distanciamentoMesa(vetMesa, numeroMesa);//Enviar pro método de distanciamento o número original da mesa, pois no método chamado nesta linha também é subtraido 1 do número da mesa
+                            //System.out.println("\nAviso: Mesa " + idMesa + " foi ocupada com sucesso!\n");
+                            break;
+                        }else{
+                            System.out.println("\nErro: A mesa solicitada não pode ser ocupada no momento! Por favor, verique se ela não está OCUPADA, RESERVADA, bloqueada por DISTANCIAMENTO_SOCIAL ou se é para DESINFECTAR.");
+                        }
+                    }
+                }
+            }
+        }else{
+            System.out.println("\nErro: O numero de mesa digitado é invalido!\n");
+        }
+        /*
         if(numeroMesa >= 0 && numeroMesa <= 30){
             if(mesaLivre(vetMesa[idMesa]) == true){
                 vetMesa[idMesa].setEstadoAtual(OCUPADA);
@@ -125,10 +148,16 @@ public class GerenciamentoDeMesasDeRestauranteGMR {
         }else{
             System.out.println("\nErro: O numero de mesa digitado é invalido!\n");
         }
+        */
     }
     
     public static void main(String[] args) {
-        Mesa vetMesa[] = new Mesa[30];
+        int i;
+        int j;
+        
+        //Mesa vetMesa[] = new Mesa[30];
+        Mesa vetMesa[][] = new Mesa[5][6];
+        int cont = 1;
         
         Mesa mesa1 = new Mesa();
         Mesa mesa2 = new Mesa();
@@ -161,6 +190,47 @@ public class GerenciamentoDeMesasDeRestauranteGMR {
         Mesa mesa29 = new Mesa();
         Mesa mesa30 = new Mesa();
         
+        vetMesa[0][0] = mesa1;
+        vetMesa[0][1] = mesa2;
+        vetMesa[0][2] = mesa3;
+        vetMesa[0][3] = mesa4;
+        vetMesa[0][4] = mesa5;
+        vetMesa[0][5] = mesa6;
+        vetMesa[1][0] = mesa7;
+        vetMesa[1][1] = mesa8;
+        vetMesa[1][2] = mesa9;
+        vetMesa[1][3] = mesa10;
+        vetMesa[1][4] = mesa11;
+        vetMesa[1][5] = mesa12;
+        vetMesa[2][0] = mesa13;
+        vetMesa[2][1] = mesa14;
+        vetMesa[2][2] = mesa15;
+        vetMesa[2][3] = mesa16;
+        vetMesa[2][4] = mesa17;
+        vetMesa[2][5] = mesa18;
+        vetMesa[3][0] = mesa19;
+        vetMesa[3][1] = mesa20;
+        vetMesa[3][2] = mesa21;
+        vetMesa[3][3] = mesa22;
+        vetMesa[3][4] = mesa23;
+        vetMesa[3][5] = mesa24;
+        vetMesa[4][0] = mesa25;
+        vetMesa[4][1] = mesa26;
+        vetMesa[4][2] = mesa27;
+        vetMesa[4][3] = mesa28;
+        vetMesa[4][4] = mesa29;
+        vetMesa[4][5] = mesa30;
+        
+        
+        for(i = 0; i < 5; i++){
+            for(j = 0; j < 6; j++){
+                vetMesa[i][j].setNumeroMesa((cont));
+                cont++;
+            }
+        }
+        
+        //System.out.println("Numero da mesa "+vetMesa[0][3].getNumeroMesa());
+        /*
         mesa1.numeroMesa = 1;
         mesa2.numeroMesa = 2;
         mesa3.numeroMesa = 3;
@@ -191,39 +261,10 @@ public class GerenciamentoDeMesasDeRestauranteGMR {
         mesa28.numeroMesa = 28;
         mesa29.numeroMesa = 29;
         mesa30.numeroMesa = 30;
+        */
         
-        vetMesa[0] = mesa1;
-        vetMesa[1] = mesa2;
-        vetMesa[2] = mesa3;
-        vetMesa[3] = mesa4;
-        vetMesa[4] = mesa5;
-        vetMesa[5] = mesa6;
-        vetMesa[6] = mesa7;
-        vetMesa[7] = mesa8;
-        vetMesa[8] = mesa9;
-        vetMesa[9] = mesa10;
-        vetMesa[10] = mesa11;
-        vetMesa[11] = mesa12;
-        vetMesa[12] = mesa13;
-        vetMesa[13] = mesa14;
-        vetMesa[14] = mesa15;
-        vetMesa[15] = mesa16;
-        vetMesa[16] = mesa17;
-        vetMesa[17] = mesa18;
-        vetMesa[18] = mesa19;
-        vetMesa[19] = mesa20;
-        vetMesa[20] = mesa21;
-        vetMesa[21] = mesa22;
-        vetMesa[22] = mesa23;
-        vetMesa[23] = mesa24;
-        vetMesa[24] = mesa25;
-        vetMesa[25] = mesa26;
-        vetMesa[26] = mesa27;
-        vetMesa[27] = mesa28;
-        vetMesa[28] = mesa29;
-        vetMesa[29] = mesa30;
         
-        //System.out.println("vetMesa[0] = " + vetMesa[0]);
+        //System.out.println("vetMesa[0][0] = " + vetMesa[0][0]);
         
         //int i;
         
@@ -244,15 +285,15 @@ public class GerenciamentoDeMesasDeRestauranteGMR {
         */
         //distanciamentoMesa(vetMesa, 15);
         
-        ocuparMesa(vetMesa, 15);
+        ocuparMesa(vetMesa, 9);
         
-        System.out.println("\nEstado da mesa 2: " + vetMesa[1].estadoAtual);
-        System.out.println("\nEstado da mesa 15: " + vetMesa[14].estadoAtual);
-        System.out.println("\nEstado da mesa 9: " + vetMesa[8].estadoAtual);
-        System.out.println("\nEstado da mesa 21: " + vetMesa[20].estadoAtual);
-        System.out.println("\nEstado da mesa 14: " + vetMesa[13].estadoAtual);
-        System.out.println("\nEstado da mesa 16: " + vetMesa[15].estadoAtual);
-        System.out.println("\nEstado da mesa 17: " + vetMesa[16].estadoAtual);
+        System.out.println("\nEstado da mesa 2: " + vetMesa[0][1].estadoAtual);
+        //System.out.println("\nEstado da mesa 15: " + vetMesa[14].estadoAtual);
+        System.out.println("\nEstado da mesa 9: " + vetMesa[1][2].estadoAtual);
+        System.out.println("\nEstado da mesa 3: " + vetMesa[0][2].estadoAtual);
+        System.out.println("\nEstado da mesa 8: " + vetMesa[1][1].estadoAtual);
+        System.out.println("\nEstado da mesa 10: " + vetMesa[1][3].estadoAtual);
+        System.out.println("\nEstado da mesa 15: " + vetMesa[2][2].estadoAtual);
         
         //mesa01.estadoAtual = DESINFECTAR;
         
